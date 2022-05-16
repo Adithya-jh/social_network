43ed31adab5c541e234dd44006d86988afb51e23
Adithya <jhadith88@gmail.com>
2022-05-16 16:07:55 +0530

    first commit

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import DiscordProvider from "next-auth/providers/discord";
+import InstagramProvider from "next-auth/providers/instagram";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      code: '/',
+      redirect_uri: 'localhost:3000'
+    }),
+
+    InstagramProvider({
+      clientId: process.env.INSTAGRAM_CLIENT_ID,
+      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET
+    })
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      session.user.tag = session.user.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+
+      session.user.uid = token.sub;
+      return session;
+    },
+  },
+});
