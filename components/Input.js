a82e1f908dcfec50262352796e80f58eb5f889e4
Adithya <jhadith88@gmail.com>
2022-06-07 10:15:48 +0530

    updtew

--- components/Input.js
+++ components/Input.js
@@ -95,7 +95,7 @@ function Input() {
           <textarea
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            placeholder="What's happening?"
+            placeholder="Start Connecting...."
             rows="2"
             className="bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px]"
           />
@@ -159,7 +159,7 @@ function Input() {
               )}
             </div>
             <button
-              className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
+              className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:opacity-50 disabled:cursor-default"
               disabled={!input && !selectedFile}
               onClick={sendPost}
             >
