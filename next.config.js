fdd0c24772d29255551748886e0017ed78ee9b03
Adithya <jhadith88@gmail.com>
2022-06-20 09:50:13 +0530

    added page purpose

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ["rb.gy"],
+    domains: ["rb.gy","www.istockphoto.com"],
   },
   reactStrictMode: true,
   i18n: {
--- pages/profile.js
+++ pages/profile.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import {motion} from 'framer-motion'
 
 function profile() {
   return (
-    <div className="text-white">This is profile section</div>
+    <motion.div 
+    animate={{ x: [-1000, 1000, 0] }}
+    // animate={{ rotate: 360 }}
+    transition={{ duration: 0.5 }}
+    className="text-white align-center justify-center flex  h-[100vh] position-absolute w-[100vw]">
+      {/* <div>
+        <Image src="https://www.istockphoto.com/illustrations/book-club" height="100vh" width="100vh"/>
+      </div> */}
+      <div className="mt-[400px]">
+         ## YOUR PROFILE WILL BE SHOWN HERE SHORTLY
+      </div>
+       </motion.div>
   )
 }
 
